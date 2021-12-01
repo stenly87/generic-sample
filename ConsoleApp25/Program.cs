a6a1283b0302a8eacc7697f53b710ad74a83e891
ppk <ppk_student@hotmail.com>
2021-12-02 09:57:39 +1000

    Add project files.

---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Обобщения 
+            
+            Human human = new Human { Name = "Эльвира" };
+            string result =  human.Serialize();
+            Console.WriteLine(result);
+            Animal<bool> animal = new Animal<bool> 
+            { Age = 10, Name = "Бобик",
+                GenericProperty = true };
+            Console.WriteLine(animal.Serialize());
+        }
+    }
+    
+    public class Human
+    { 
+        public string Name { get; set; }
+        private double Weight = 10;
+    }
+
+    public class Animal<T>
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public T GenericProperty { get; set; }
+    }
+
+    public static class Extension
+    {
+        public static string Serialize<T>(this T obj)
+        {
+            string result = null;
+            DataContractJsonSerializer jsonSerializer =
+                   new DataContractJsonSerializer(typeof(T));
+            using (var ms = new MemoryStream())
+            using (var br = new BinaryReader(ms))
+            {
+                jsonSerializer.WriteObject(ms, obj);
+                ms.Seek(0, SeekOrigin.Begin);
+                result = Encoding.UTF8.GetString(br.ReadBytes((int)ms.Length));
+            }
+            return result;
+        }
+    }
+}
